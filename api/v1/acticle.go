dca2b0b8dcc0375644dc800cd5d56cee939b5026
PlutuWu-Cn <p1utowu@qq.com>
2021-06-16 20:34:17 +0800

    增加验证接口。构建功能接口，User完成

---
+++ api/v1/acticle.go
@@ -0,0 +1,18 @@
+package v1
+
+// AddArticle 添加文章
+
+
+// GetCateArt 查询分类下的所有文章
+
+
+// GetArtInfo 查询单个文章信息
+
+
+// GetArt 查询文章列表
+
+
+// EditArt 编辑文章
+
+
+// DeleteArt 删除文章
--- routes/router.go
+++ routes/router.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"p-blog/utils"
 )
 
@@ -10,13 +9,25 @@ func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("api/v1")
+	/*
+		后台管理路由接口
+	*/
+	auth := r.Group("api/v1")
 	{
-		router.GET("hello", func (c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H {
-				"msg" : "ok",
-			})
-		})
+		// 用户模块的路由接口
+
+		//修改密码
+
+		// 分类模块的路由接口
+
+		// 文章模块的路由接口
+
+		// 上传文件
+
+		// 更新个人设置
+
+		// 评论模块
+
 	}
 
 	r.Run(utils.HttpPort)
